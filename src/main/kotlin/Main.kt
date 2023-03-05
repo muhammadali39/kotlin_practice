e33a15e9280bc1948b2967c3f81277fda8f63119
Mateen PC <muhammadumairali987@gmail.com>
2023-03-05 22:24:49 +0500

    today(05032023)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,14 +8,40 @@ fun main(){
  //   name = ""
 
 
+//    println(name)
+//    println(greeting)
+//
+//    greeting = null
+//    greeting = "hello"
+//
+//
+//    println(name)
+//    println(greeting)
+
+//    if(greeting!=null){
+//        println(greeting)
+//    }else{
+//        println("Hi")
+//    }
+
+//    greeting = "hello"
+//    when(greeting){
+//        null-> println("hi")
+//        else-> println(greeting)
+//    }
+
+//    greeting = "hello"
+//    val greetingtoprint = if(greeting != null) greeting else "Hi"
+//     println(greetingtoprint)
+
+
+       greeting="hello"
+    val greetingtoprint = when(greeting){
+        null->"Hi"
+        else->greeting
+    }
+    println(greetingtoprint)
+
     println(name)
-    println(greeting)
-
-    greeting = null
-    greeting = "hello"
-
-
-    println(name)
-    println(greeting)
 
 }
